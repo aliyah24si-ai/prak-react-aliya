31eec2c71690e11620af228ce0aeb91fd241e7b3
aliyah <aliyah24si@mahasiswa.pcr.ac.id>
2026-04-09 21:18:45 +0700

    hapus folder latihan

--- src/latihanp3/main.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./tailwind.css";
-import BiayaKirimForm from "./BiayaKirimForm";
-
-createRoot(document.getElementById("root")).render(<BiayaKirimForm />);
--- src/latihanp4/main.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./tailwind.css";
-import MovieList from "./MovieList";
-
-createRoot(document.getElementById("root")).render(<MovieList />);
