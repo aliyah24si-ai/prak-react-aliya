4a2c551ca00f902019b754da63fb61ae20c0af0f
aliyah <aliyah24si@mahasiswa.pcr.ac.id>
2026-05-04 16:48:39 +0700

    fix: hapus konfigurasi latihanp2 yang bikin error

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], 
-         
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        latihanp2: resolve(__dirname, 'latihanp2.html'),
-        latihanp3: resolve(__dirname, 'latihanp3.html'),
-        latihanp4: resolve(__dirname, 'latihanp4.html'),
-        'pertemuan-5': resolve(__dirname, 'pertemuan-5.html'),
-      },
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
